03e0eecaf102a611755c61caf0fe192bce0e6788
Sparsh Tandon <snowmansparsh4@gmail.com>
2023-05-06 16:39:27 +0530

    user details update api added

--- routes/paths.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const controller = require('./index');
-router.post("/signup", controller.signup)
-router.post("/signin", controller.signin)
-router.post("/user/update", controller.updateUserDetails)
-
-module.exports = router;
