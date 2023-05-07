297676f4b81ea2df22a4186cad2f130e6be78b33
codesparsh <snowmansparsh4@gmail.com>
2023-05-07 16:13:03 +0530

    list feeds and sanitation api added

--- server/routes/index.js
+++ server/routes/index.js
@@ -69,11 +69,19 @@ module.exports.signin = async (req, res) => {
                     "error": "User not found, please sign in"
                 })
             } else {
-                let newUser = JSON.parse(user)
-                res.send({
-                    "status": "Success",
-                    "user": newUser
-                })
+                if (password == user.password) {
+                    let newUser = JSON.parse(user)
+                    res.send({
+                        "status": "Success",
+                        "user": newUser
+                    })
+                } else {
+                    res.send({
+                        "status": "Failure",
+                        "error": "Incorrect Password"
+                    })
+                }
+                
             }
         }).catch(() => {
             res.send({
@@ -103,6 +111,7 @@ module.exports.updateUserDetails = async (req, res) => {
     let species = req.body.species
     let shedDimensions = req.body.shedDimensions
     let state = req.body.state
+    let lastSanitation = req.body.sanitation
 
     let key = USER_KEY + ":" + username
     await redisClient.get(key).then(async user => {
@@ -115,10 +124,11 @@ module.exports.updateUserDetails = async (req, res) => {
             let newUser = JSON.parse(user)
             let updatedUser = {
                 ...newUser,
-                tiers: tiers,
-                species: species,
-                shedDimensions: shedDimensions,
-                state: state
+                tiers: (tiers == null || tiers == undefined) ? newUser.tiers : tiers,
+                species: (species == null || species == undefined) ? newUser.species : species,
+                shedDimensions: (shedDimensions == null || shedDimensions == undefined) ? newUser.shedDimensions : shedDimensions,
+                state: (state == null || state == undefined) ? newUser.state : state,
+                sanitation: (lastSanitation == null || lastSanitation == undefined) ? newUser.lastSanitatedAt : lastSanitation
             }
 
             redisClient.set(key, value = JSON.stringify(updatedUser)).then(() => {
@@ -142,19 +152,45 @@ module.exports.updateUserDetails = async (req, res) => {
     })
 }
 
-module.exports.getFeedData = async (req, res) => {
+
+module.exports.listFeeds = async (req, res) => {
     let username = req.body.username
     let userDetailsKey = USER_KEY + ":" + username
-    redisClient.get(userDetailsKey).then(async (user) => {
-        let channelId = process.env.CHANNEL_ID
-        redisClient.get()
+    await redisClient.get(userDetailsKey).then(async (user) => {
+        if (user != null || user != undefined) {
+            let channelId = process.env.CHANNEL_ID
+            let key = FARM_KEY + ":" + channelId
+            await redisClient.get(key).then((data) => {
+                let parsedData = JSON.parse(data)
+                let feedArr = (parsedData.feeds == null || parsedData.feeds == undefined) ? [] : parsedData.feeds
+                res.send({
+                    "status": "Success",
+                    "feeds": feedArr
+                })
+            }).catch(() => {
+                res.send({
+                    "status": "Failure",
+                    "error": "DB Error"
+                })
+            })
+        } else {
+            res.send({
+                "status": "Failure",
+                "error": "User not found"
+            })
+        }
+
     }).catch(err => {
-        console.log("User not found")
+        console.log("Server Error")
+        res.send({
+            "status": "Failure",
+            "error": "DB Error"
+        })
     })
 }
 
-cron.schedule("*/30 * * * * *", () => {
-    console.log("Cron Job runing every second")
+cron.schedule("*/10 * * * * *", () => {
+    console.log("Cron Job runing every 10 second")
     http.get(`http://api.thingspeak.com/channels/${process.env.CHANNEL_ID}/feeds.json?api_key=${process.env.API_KEY}&results=1`, (response) => {
 
         let data = '';
@@ -163,42 +199,48 @@ cron.schedule("*/30 * * * * *", () => {
             data += chunk;
         });
 
-        response.on('end', async () => {
+        response.on('end', () => {
             console.log('Retrieved all data');
             let parsedData = JSON.parse(data);
-            console.log(parsedData);
             let channelId = parsedData.channel.id
             let feed = parsedData.feeds.length > 0 ? parsedData.feeds[0] : undefined
             let temperature = feed.field1
             let humidity = feed.field2
-            console.log(temperature, humidity)
+
+            console.log({ temperature: temperature, humidity: humidity })
+
             let key = FARM_KEY + ":" + channelId
-            let feedData = {
-                "entry": feed.entry_id,
-                "created_at": feed.created_at,
-                "temperature": temperature,
-                "humidity": humidity
-            }
             let feedArr = []
-            await redisClient.get(key).then(feedArrData => {
+
+            redisClient.get(key).then(feedArrData => {
                 if (feedArrData != null || feedArrData != undefined) {
-                    feedArr = JSON.parse(feedArrData)
+                    parsedFeedData = JSON.parse(feedArrData)
+                    feedArr = (parsedFeedData.feeds == null || parsedFeedData.feeds == undefined) ? [] : parsedFeedData.feeds
                 } else {
                     feedArr = []
                 }
-                console.log("---------   " + feedArrData)
+
+
+                let findEntry = feedArr.find((entry) => entry.entry_id == feed.entry_id)
+                if (findEntry == null || findEntry == undefined) {
+                    feedArr.push({
+                        "entry_id": feed.entry_id,
+                        "created_at": feed.created_at,
+                        "temperature": temperature,
+                        "humidity": humidity
+                    })
+                }
+
+                redisClient.set(key, value = JSON.stringify({ "feeds": feedArr })).then(() => {
+                    console.log("New Feed Update - ID:" + feed.entry_id)
+                }).catch(() => {
+                    console.log("Cannot update feed in db - ID:" + feed.entry_id)
+                })
             }).catch(err => {
-                feedArr = []
+                console.log(err)
             })
 
-            // feedArr = feedArr.push(feedData)
-            console.log("---------   " + feedArr)
 
-            redisClient.set(key, value = JSON.stringify(feedArr)).then(() => {
-                console.log("New Feed Update - ID:" + feed.entry_id)
-            }).catch(() => {
-                console.log("Cannot update feed in db - ID:" + feed.entry_id)
-            })
         })
     })
 })
--- server/routes/paths.js
+++ server/routes/paths.js
@@ -3,5 +3,6 @@ const controller = require('./index');
 router.post("/signup", controller.signup)
 router.post("/signin", controller.signin)
 router.post("/user/update", controller.updateUserDetails)
+router.post("/list/feeds", controller.listFeeds)
 
 module.exports = router;
